a665a6f1a89237483d142885a90c0f190001a296
Kirucoderanger <klemu@byupathway.edu>
2025-04-16 18:35:59 +0300

    challenges js

---
+++ spadeJS/scripts/challenges.js
@@ -0,0 +1,34 @@
+
+  
+    
+    const themeToggle = document.getElementById('themeToggle');
+            const body = document.body;
+    
+            themeToggle.addEventListener('click', () => {
+                if (body.classList.contains('light-mode')) {
+                    body.classList.remove('light-mode');
+                    body.classList.add('dark-mode');
+                    themeToggle.textContent = 'Light Mode';
+                } else {
+                    body.classList.remove('dark-mode');
+                    body.classList.add('light-mode');
+                    themeToggle.textContent = 'Dark Mode';
+                }
+            });
+            // Set dark mode as the default theme
+            body.classList.add('dark-mode');
+            themeToggle.textContent = 'Light Mode';
+            
+    const hamButton = document.querySelector('#menu');
+    const navigation = document.querySelector('.navigation');
+    
+    hamButton.addEventListener('click', () => {
+        navigation.classList.toggle('open');
+        hamButton.classList.toggle('open');
+    });
+
+
+   
+    
+    
+
